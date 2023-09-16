8629327fd5d60d1d976933600b6fbb44a51d5e56
k_h <K_Haruka03@outlook.jp>
2023-09-17 00:46:47 +0900

    自己紹介部分の作成

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Header from './components/Header';
+import Introduction from './components/Introduction';
+import { VStack } from '@chakra-ui/react';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <VStack>
+        <Introduction />
+      </VStack>
     </div>
   );
 }
