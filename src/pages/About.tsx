089286bf797c166318a8f45d55c80e84bdade768
k_h <K_Haruka03@outlook.jp>
2023-09-27 03:11:50 +0900

    ページ遷移の実装

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import '../App.css';
+import Header from '../components/Header';
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <Header />
+    </div>
   )
 }
 
