0735c4a3bec1e9d57802b85b627496a35422f29b
k_h <K_Haruka03@outlook.jp>
2023-10-03 21:18:53 +0900

    開発物情報をより詳細に変更

--- src/components/DevPart.tsx
+++ src/components/DevPart.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import { Text, VStack } from '@chakra-ui/layout'
+import { HStack, Text, VStack } from '@chakra-ui/layout'
 import { Image } from '@chakra-ui/image'
+import { FaHashtag } from 'react-icons/fa'
 
 type Props = {
     image: string
     title: string
     detail: string
+    date: string
+    skills: string
 }
 
 const DevPart = (props: Props) => {
@@ -14,7 +17,7 @@ const DevPart = (props: Props) => {
         margin={15}
         bg='white' 
         w='200px' 
-        h='230px'
+        h='260px'
         borderRadius='10px'
     >
         <Image
@@ -26,20 +29,34 @@ const DevPart = (props: Props) => {
             src={"dev/" + props.image}
             objectFit='cover'
         />
-        <Text margin={0} fontSize='20px' fontWeight='bold'>
+        <Text 
+            marginLeft='8px' 
+            marginY={0} w='100%'
+            h='5px' 
+            textAlign='left'
+            fontSize='10px'
+            lineHeight='3px'
+        >
+            {props.date}
+        </Text>
+        <Text margin={0} h='30px' fontSize='20px' fontWeight='bold'>
             {props.title}
         </Text>
         <Text 
             margin={0} 
-            fontSize='17px' 
+            fontSize='15px' 
             w='90%' 
-            h='60px'
+            h='55px'
             lineHeight='18px' 
             wordBreak='break-all'
             overflow='hidden'
         >
             {props.detail}
         </Text>
+        <HStack marginLeft='20px' marginTop='3px' w='100%' h='20px' alignItems='start'>
+            <FaHashtag size='15px' color='#A69A7D'/>
+            <Text margin={0} w='100%' lineHeight='15px' color='#A69A7D'>{props.skills}</Text>
+        </HStack>
     </VStack>
   )
 }
