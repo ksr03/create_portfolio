e41be5c9ff012af17c70546958baf562fa2676d6
k_h <K_Haruka03@outlook.jp>
2023-10-18 01:19:05 +0900

    絶対パスへの書き換え

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
-import Header from '../components/Header';
-import Top from '../components/Top';
-import About from '../components/About';
-import Developments from '../components/Developments';
-import Copyright from '../components/Copyright';
-import Loading from '../components/Loading';
+import Header from '@components/Header';
+import Top from '@components/Top';
+import About from '@components/About';
+import Developments from '@components/Developments';
+import Copyright from '@components/Copyright';
+import Loading from '@components/Loading';
 import { VStack } from '@chakra-ui/react';
 
 const Home = () => {
